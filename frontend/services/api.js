2449faabb16cf898cc5ba6daee0c6a8504b70d3a
nexovate335 <nexovate335@gmail.com>
2025-09-04 12:25:05 +0100

    j'ai supprimé

--- frontend/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // ton backend tourne ici
-  headers: { 'Content-Type': 'application/json' }
-});
-
-export default api;
